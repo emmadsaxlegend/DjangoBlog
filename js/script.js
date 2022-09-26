d12a5e0643d244626d8bc66452624302167971de
Emmanuel Okereke <eokereke47@gmail.com>
2022-09-26 21:26:52 +0100

    post article

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ $('.post-wrapper').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 4000,
     nextArrow: $('.next'),
     prevArrow: $('.prev'),
     responsive: [
